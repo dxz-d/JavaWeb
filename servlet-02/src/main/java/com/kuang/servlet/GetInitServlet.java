9f75b3ae055f852368bb838dd81adf05d03e9994
dxz-d <1205760997@qq.com>
2020-08-19 15:39:01 +0800

    Servlet获取初始化参数

---
+++ servlet-02/src/main/java/com/kuang/servlet/GetInitServlet.java
@@ -0,0 +1,27 @@
+package com.kuang.servlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @description  获取初始化参数
+ * @author diaoxiuze
+ * @date 2020/8/19 15:28
+ */  
+public class GetInitServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContext servletContext = this.getServletContext();
+        String url = servletContext.getInitParameter("url");
+        resp.getWriter().println(url);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
